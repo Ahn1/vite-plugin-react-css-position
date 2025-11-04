d742a161187f7081c1216405c67561f22fe14d3c
Alex <alexanderb@posteo.de>
2025-11-04 15:40:53 +0100

    Refactor viteCustomCssPosition to streamline CSS plugin integration and enhance code readability

--- src/viteCustomCssPosition.ts
+++ src/viteCustomCssPosition.ts
@@ -14,39 +14,41 @@ export default function viteCustomCssPosition(
   const globalVarName = `__vite_c_css_pos_initial`;
   const eventName = `__vite_c_css_pos_update`;
 
+  const cssPlugin = cssInjectedByJsPlugin({
+    dev: {
+      enableDev: options?.enableDev ?? false,
+      removeStyleCode(id: string) {
+        return `
+    (() => {
+          if(window.${globalVarName} && window.${globalVarName}.has('${id}')) {
+            window.${globalVarName}.delete('${id}');
+            window.dispatchEvent( new Event('${eventName}') );
+          }
+    })()
+          `;
+      },
+    },
+    injectCode: (css, attributes) => {
+      const attributesString = JSON.stringify(attributes.attributes);
+      const id = `"${
+        attributes.attributes?.["data-vite-dev-id"] ?? "__nokey__"
+      }"`;
+      return `
+    const css = ${css};
+    const id = ${id};
+      const attributes = JSON.parse('${attributesString}');
+      window.${globalVarName} = window.${globalVarName} || new Map();
+      window.${globalVarName}.set(id, {css, attributes});
+
+      window.dispatchEvent( new Event('${eventName}') );
+    `;
+    },
+  });
+
   return [
     {
       name: "vite-plugin-custom-css-position",
-      ...cssInjectedByJsPlugin({
-        dev: {
-          enableDev: options?.enableDev ?? false,
-          removeStyleCode(id: string) {
-            return `
-          (() => {
-                if(window.${globalVarName} && window.${globalVarName}.has('${id}')) {
-                  window.${globalVarName}.delete('${id}');
-                  window.dispatchEvent( new Event('${eventName}') );
-                }
-          })()
-                `;
-          },
-        },
-        injectCode: (css, attributes) => {
-          const attributesString = JSON.stringify(attributes.attributes);
-          const id = `"${
-            attributes.attributes?.["data-vite-dev-id"] ?? "__nokey__"
-          }"`;
-          return `
-          const css = ${css};
-          const id = ${id};
-            const attributes = JSON.parse('${attributesString}');
-            window.${globalVarName} = window.${globalVarName} || new Map();
-            window.${globalVarName}.set(id, {css, attributes});
-    
-            window.dispatchEvent( new Event('${eventName}') );
-          `;
-        },
-      }),
-    } satisfies Plugin,
+    },
+    ...(Array.isArray(cssPlugin) ? cssPlugin : [cssPlugin]),
   ];
 }
