f650c462c70833bbc3a4257da566338ddda1f081
Alex <alexanderb@posteo.de>
2025-11-04 15:00:54 +0100

    restructure

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   // ...config options
-})
+  external: ["lighningcss"],
+  define: {
+    "process.env.CSS_TRANSFORMER_WASM": "false",
+  },
+});
